83fa767dc77b411bc185eb5b5b9b3dfd69cedd2e
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-09-09 23:29:36 +0800

    testing the crowd funding API with docker and pytest

--- backend/app/db/tasks.py
+++ backend/app/db/tasks.py
@@ -1,18 +1,20 @@
+import os
 from fastapi import FastAPI
 from databases import Database
-import logging
-
 from app.core.config import DATABASE_URL
+import logging
 
 logger = logging.getLogger(__name__)
 
 
 async def connect_to_db(app: FastAPI) -> None:
-    database = Database(DATABASE_URL, min_size=2, max_size=10)
+
+    DB_URL = f"{DATABASE_URL}_test" if os.environ.get(
+        "TESTING") else DATABASE_URL
+    database = Database(DB_URL, min_size=2, max_size=10)
 
     try:
         await database.connect()
-
         app.state._db = database
     except Exception as e:
         logger.warn("--- DB CONNECTION ERROR ---")
@@ -24,6 +26,6 @@ async def close_db_connection(app: FastAPI) -> None:
     try:
         await app.state._db.disconnect()
     except Exception as e:
-        logger.warn("--- DB DISCONNECTION ERROR ---")
+        logger.warn("--- DB DISCONNECT ERROR ---")
         logger.warn(e)
-        logger.warn("--- DB DISCONNECTION ERROR ---")
+        logger.warn("--- DB DISCONNECT ERROR ---")
